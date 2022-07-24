9e2191929adeacc46d05b2628ec27406b607cdac
Guilhem Saurel <guilhem.saurel@laas.fr>
2022-07-24 17:06:59 +0200

    async chat consumer

--- homechan/asgi.py
+++ homechan/asgi.py
@@ -11,14 +11,21 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "homechan.settings")
 
 django_asgi_app = get_asgi_application()
 
+import chat.routing  # noqa: E402
+
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns))
+        ),
     }
 )
