4b4a2fcee221a5e6710425360c03559a84e0acb0
Guilhem Saurel <guilhem.saurel@laas.fr>
2022-07-24 16:09:35 +0200

    install channels

--- homechan/settings.py
+++ homechan/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "channels",
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,4 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+ASGI_APPLICATION = "homechan.asgi.application"
